7288d1f6de19a3203359be862d30ec6df371f12d
jetonpeche <nicolas.np63@gmail.com>
2025-12-07 17:22:33 +0100

    fix traductionPipe

--- src/lib/traductionPipe.ts
+++ src/lib/traductionPipe.ts
@@ -10,23 +10,23 @@ export class TraductionPipe implements PipeTransform
     private tradServ = inject(TraductionService);
 
   transform(value: unknown, ...args: unknown[]): unknown 
-  { 
+  {
     let phrase = this.tradServ.get(value as string);
 
     if(args.length == 0)
       return phrase;
   
-    const VALEUR = args[0] as string;
+    const VALEUR = args[0];
 
-    if(VALEUR == null || VALEUR == undefined)
-      return phrase;
+    if(VALEUR instanceof Date)
+      return phrase.replace("{var}", args[0].toLocaleString().split(" ")[0]); 
 
-    if(VALEUR.match(/^\d{4}-\d{2}-\d{2}$/))
+    if(typeof VALEUR == "string" && VALEUR.match(/^\d{4}-\d{2}-\d{2}$/))
     {
       let date = new Date(VALEUR).toLocaleString().split(" ")[0];
       return phrase.replace("{var}", date);
     }
 
-    return phrase.replace("{var}", (args[0] as Date).toLocaleString().split(" ")[0]); 
+    return phrase.replace("{var}", VALEUR.toString());
   }
 }
